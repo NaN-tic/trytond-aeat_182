53c4b9170cbca8a91935b3b95af4ff80fb064bdb
jmartin <jmartin@zikzakmedia.com>
2016-04-21 13:57:30 +0200

    Downgrade tests

--- tests/test_aeat_182.py
+++ tests/test_aeat_182.py
@@ -1,12 +1,11 @@
 # This file is part of aeat_182 module for Tryton.
 # The COPYRIGHT file at the top level of this repository contains
 # the full copyright notices and license terms.
-from trytond.tests.test_tryton import ModuleTestCase
-from trytond.tests.test_tryton import doctest_checker
-from trytond.tests.test_tryton import doctest_setup, doctest_teardown
-from trytond.tests.test_tryton import suite as test_suite
-import doctest
 import unittest
+import doctest
+import trytond.tests.test_tryton
+from trytond.tests.test_tryton import ModuleTestCase
+from trytond.tests.test_tryton import doctest_setup, doctest_teardown
 
 
 class Aeat182TestCase(ModuleTestCase):
@@ -15,11 +14,10 @@ class Aeat182TestCase(ModuleTestCase):
 
 
 def suite():
-    suite = test_suite()
+    suite = trytond.tests.test_tryton.suite()
     suite.addTests(unittest.TestLoader().loadTestsFromTestCase(
             Aeat182TestCase))
     suite.addTests(doctest.DocFileSuite('scenario_aeat_182.rst',
             setUp=doctest_setup, tearDown=doctest_teardown, encoding='utf-8',
-            checker=doctest_checker,
             optionflags=doctest.REPORT_ONLY_FIRST_FAILURE))
     return suite
